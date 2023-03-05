41ceec5ba385d10dd5b93ae58ef073df7759466f
seungsu <h970126@gmail.com>
2023-03-05 21:45:13 +0900

    #7 - domain class (Article, ArticleComment)

---
+++ src/main/java/com/example/forum/domain/Article.java
@@ -0,0 +1,15 @@
+package com.example.forum.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String content;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
