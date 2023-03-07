7c345616bdcf0f017914969d2f15b9b0752ed8b9
seungsu <h970126@gmail.com>
2023-03-07 23:55:10 +0900

    #13 - rest repositories, HAL explorer
    setting & test

--- src/main/java/com/example/forum/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/forum/repository/ArticleCommentRepository.java
@@ -2,8 +2,8 @@ package com.example.forum.repository;
 
 import com.example.forum.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/test/java/com/example/forum/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/forum/repository/JpaRepositoryTest.java
@@ -6,7 +6,6 @@ import com.example.forum.domain.ArticleComment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Commit;
