793e04310f5d1efa86f2b679318c451bbf30141c
seungsu <h970126@gmail.com>
2023-03-15 01:23:14 +0900

    [Refactor] : RestSource Annotation

--- migration-to-kotlin/src/main/kotlin/com/example/practice/repository/UserRepository.kt
+++ migration-to-kotlin/src/main/kotlin/com/example/practice/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package com.example.practice.repository
 
 import com.example.practice.domain.UserAccount
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RestResource
 
+@RestResource
 interface UserRepository : JpaRepository<UserAccount, Long> {
     fun findByUserId(userId : String) : UserAccount?
 }
