1193a2f6186dea96b474688af0f9b3395af76a3f
황승수 (Seungsu) <h970126@gmail.com>
2023-04-10 19:24:34 +0900

    #30 - 정렬 기능 추가

--- forum/src/main/java/com/example/forum/service/ArticleCommentService.java
+++ forum/src/main/java/com/example/forum/service/ArticleCommentService.java
@@ -1,14 +1,18 @@
 package com.example.forum.service;
 
+import com.example.forum.domain.ArticleComment;
 import com.example.forum.dto.ArticleCommentDto;
 import com.example.forum.repository.ArticleCommentRepository;
 import com.example.forum.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -18,18 +22,32 @@ public class ArticleCommentService {
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComment(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
-
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패 - {}", dto);
+        }
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
-
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) {
+                articleComment.setContent(dto.content());
+            }
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패 - {}", dto);
+        }
     }
 
     public void deleteArticleComment(Long articleCommentId) {
-
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
